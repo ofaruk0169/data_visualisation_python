bae07698e2683d9e119a07f13ac4af6a8a313452
Omar Faruk <ofaruk0169@gmail.com>
2020-03-17 15:18:18 +0000

    cool stuff added

---
+++ colormee.py
@@ -0,0 +1,11 @@
+name = input ('What is your name?' )
+color = input ('what is the best color?')
+
+print (f"hell {name} I see your favourite color is {color}")
+
+print("Hello " + name + " I see your favourite color is " + color)
+
+birthday = input ('what is your birthyeear?')
+age = 2019 - int(birthday)
+
+print(age)
